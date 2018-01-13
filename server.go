1044d05c89c831ad8b37f3e8040559e478f9d882
gudbrandtandberg <gudbrandduff@gmail.com>
2018-01-13 11:27:03 -0800

    two servers

--- server.go
+++ server.go
@@ -17,24 +17,26 @@ import (
 func main() {
 	//	http.HandleFunc("/", handler) // each request calls handler
 	http.Handle("/", http.FileServer(http.Dir("./www")))
+
 	fmt.Println("HTTP server listening on port 12345")
-	log.Fatal(http.ListenAndServe(":12345", nil))
+	go func() {
+		log.Fatal(http.ListenAndServe(":12345", nil))
+	}()
 
 	s := &http.Server{
 		Addr:           ":12346",
-		Handler:        handler,
+		Handler:        http.HandlerFunc(handler),
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	log.Fatal(s.ListenAndServe())
 	fmt.Println("Listening on port 12346")
+	log.Fatal(s.ListenAndServe())
 }
 
 // handler echoes the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-	fmt.Fprintf(w, "This is our site")
+	fmt.Fprintf(w, time.Now().Format("Mon Jan _2 2006 15:04:05"))
 }
 
 //!-
