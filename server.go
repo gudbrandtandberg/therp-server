81d6bfdd423e2b5ea017ff1fed54a2518d4bbc8c
gudbrandtandberg <gudbrandduff@gmail.com>
2018-01-14 09:05:07 -0800

    kinda workin

--- server.go
+++ server.go
@@ -8,26 +8,26 @@
 package main
 
 import (
-//	"strings"
+	//	"strings"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
-	"os/exec"
 )
 
 func main() {
 	//	http.HandleFunc("/", handler) // each request calls handler
-//	http.Handle("/", http.FileServer(http.Dir("./www")))
+	//	http.Handle("/", http.FileServer(http.Dir("./www")))
 
-//	fmt.Println("HTTP server listening on port 12345")
-//	go func() {
-//		log.Fatal(http.ListenAndServe(":12345", nil))
-//	}()
+	//	fmt.Println("HTTP server listening on port 12345")
+	//	go func() {
+	//		log.Fatal(http.ListenAndServe(":12345", nil))
+	//	}()
 
 	s := &http.Server{
-		Addr:           ":12345",
-		Handler:        http.HandlerFunc(handler),
+		Addr:    ":12345",
+		Handler: http.HandlerFunc(handler),
+		//Handler:        http.FileServer(http.Dir("./www/")),
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
@@ -39,17 +39,17 @@ func main() {
 // handler echoes the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
 	timeNow := time.Now().Format("Mon Jan _2 2006 15:04:05")
-//	timeNow = strings.Replace(timeNow, " ", "", -1)
-//	fmt.Fprintf(w, timeNow )
-	//cmd := exec.Command("raspistill", "-o", "www/img/"+timeNow+".jpg")
-	cmd := exec.Command("raspistill", "-t", "1", "-q", "10", "-o", "www/img/most_recent.jpg")
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("command raspistill failed")
-	}	
-	//exec.Command("raspistill -o "+timeNow+".jpg")
+
+	// // Take a picture on the raspberry pi
+	// cmd := exec.Command("raspistill", "-t", "1", "-q", "10", "-o", "www/img/most_recent.jpg")
+	// err := cmd.Run()
+	// if err != nil {
+	// 	fmt.Println("command raspistill failed")
+	// }
+
 	fmt.Println("Execed command at ", timeNow)
-	http.ServeFile(w, r, "www/index.html")
+	http.ServeFile(w, r, "./www/index.html")
+	//http.FileServer(http.Dir("./www/")),
 }
 
 //!-
